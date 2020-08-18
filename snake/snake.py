b91e5d470bd42df6a6691b8c4f66f1c31ce3bc50
Oswaldo Spadari <oswaldo@spadari.com.br>
2020-08-18 20:56:00 -0300

    jogo snake

--- snake/snake.py
+++ snake/snake.py
@@ -1,1 +1,25 @@
-print (Hello World!)
+import pygame, random
+from pygame.locals import *
+
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('Snake')
+
+snake = [(200,200), (210, 200), (220,200)]
+my_direction = LEFT
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+
+    screen.fill((0,0,0))
+
+    pygame.display.update()
